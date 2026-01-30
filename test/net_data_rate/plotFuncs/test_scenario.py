89496c79e17299cb7de01b9061d5894bb17c0526
devarshi-balu <studious.devarshibalu@gmail.com>
2026-01-30 19:48:50 +0530

    add test folder for net_data_rate_function,  plotting scripts

---
+++ test/net_data_rate/plotFuncs/test_scenario.py
@@ -0,0 +1,25 @@
+import numpy as np
+from secnario_plot import plot_network_scenario
+# Import the function you wrote
+# from your_module import plot_network_scenario
+import numpy as np
+
+associations = {
+    0: [3, 4],
+    1: [5, 6, 7],
+    2: [8]
+}
+
+pos = np.array([
+    [0.0, 0.0],
+    [4.0, 0.0],
+    [2.0, 3.0],
+    [0.5, 0.8],
+    [-0.6, 1.2],
+    [3.6, 0.7],
+    [4.5, 1.1],
+    [3.2, -0.4],
+    [2.1, 2.4],
+])
+
+plot_network_scenario(associations, pos)
