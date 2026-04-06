cf041296200b4b512d596c662434e4cfd8628ee3
Devarshi Balu <studious.devarshibalu@gmail.com>
2026-04-06 17:32:41 +0530

    @06-04-2026

--- agent_simulations/hier_agent_test_softmax.py
+++ agent_simulations/hier_agent_test_softmax.py
@@ -3,78 +3,87 @@ import jax.numpy as jnp
 import jax
 import matplotlib.pyplot as plt
 from mapc_mab.agents.mapc_agent_factory import MapcAgentFactory
-from reinforced_lib.agents.mab import EGreedy, Softmax, UCB, NormalThompsonSampling
+from reinforced_lib.agents.mab import EGreedy, Softmax, UCB, ThompsonSampling
 from tqdm import tqdm
 from mapc_mab.envs.static_scenarios import StaticScenario
+import os
 
 import sys 
-sys.path.append("C:/Users/jomon/Documents/wifi_9/csr_mab_mlo")
 
-from pre_def_tools.pprint_dict import pprint_one_level_dict
+n_tx_power_levels: int = 12
 
-scenario = simple_scenario_5(d_ap=20, d_sta=2, mcs=11)
-total_steps = 10000
-agent_name = "softmax"
+scenario = simple_scenario_5(d_ap=40, d_sta=2, mcs=11, n_tx_power_levels=n_tx_power_levels)
+total_steps = 10_000
+agent_name = "Softmax"
 
 agent_factory = MapcAgentFactory(
     associations=scenario.associations,
     agent_type=Softmax,
     agent_params_lvl1={
-        "lr": 4.777361142106584,
-        "tau": 4.2060081652237,
-        "alpha": 0.877390137202597,
+        "lr": 0.43664735929796333,
+        "tau": 0.23426581058204046,
+        "alpha": 0.9695846277645586,
     },
     agent_params_lvl2={
-        "lr": 0.0799692352052877,
-        "tau": 0.25733916341257085,
-        "alpha": 0.4839043560610401,
+        "lr": 2.1154290797261215,
+        "tau": 7.56829206016762,
+        "alpha": 0.8948273504276488,
     },
     agent_params_lvl3={
-        "lr": 0.05531470397739702,
-        "tau": 1.5416909981725293,
-        "alpha": 0.5777131081264169,
+        "lr": 0.6218704727769077,
+        "tau": 6.978281265126034,
+        "alpha": 0.0884925020519195,
     },
     agent_params_lvl4={
-        "lr": 1.5347490932205834,
-        "tau": 4.823526476233721,
-        "alpha": 0.9773311575522818,
+        "lr": 0.03872118032174583,
+        "tau": 0.12315571723666023,
+        "alpha": 0.32533033076326434,
     },
-    hierarchical=True
+    tx_power_levels=n_tx_power_levels,
 )
 
 agent = agent_factory.create_hierarchical_mapc_agent()
+
 
 throughput = [200]
 
 key = jax.random.PRNGKey(seed=42)
 
 # window size for smoothing
-window = 200
 
-for i in tqdm(range(1, total_steps)):
+for i in tqdm(range(1, total_steps+1)):
     key, run_key = jax.random.split(key)
     link_ap_sta = agent.sample(throughput[-1])
     data_rate = scenario.__call__(run_key, link_ap_sta)
-    # print("=" * 60)
-    # print(f"at step -> prev reward = {throughput[-1]}")
-    # for link in link_ap_sta.values(): 
-    #     print("-" * 60)
-    #     print(f"link = {link}")
-    #     pprint_one_level_dict(link)
-    #     print("-" * 60)
-    # print("=" * 60)
     throughput.append(data_rate)
 
-smoothed = [jnp.mean(jnp.array(throughput[i-window:i])) for i in range(window, len(throughput))]
-# smoothed = jnp.array(throughput)
+os.makedirs(f"arrays/{agent_name}", exist_ok=True)
+jnp.save(f"arrays/{agent_name}/{agent_name}_4l_{total_steps}sm_{n_tx_power_levels}txpl.npy", throughput)
+for window in [10, 20, 30, 40, 50, 60, 100, 200, 400, 500, 600]:
+   
+    print("=" * 60)
+    print(f"window size = {window}")
+    print("-" * 30)
 
-plt.figure(figsize=(10, 8))
-plt.plot(jnp.arange(len(smoothed)), smoothed,linewidth=2)
-plt.xlabel('Steps', fontsize=14)
-plt.ylabel('Average Throughput (Mbps)', fontsize=14)
-plt.title(f"{agent_name}_4Level",  fontsize=16)
-plt.legend(fontsize=12, loc="lower right", frameon=True)
-plt.grid(True, which='both', linestyle=':', linewidth=0.7, alpha=0.7)
-plt.tight_layout()
-plt.savefig(f"{agent_name}_4Level", dpi=600, bbox_inches='tight')
-plt.show()
+    throughput_arr = jnp.asarray(throughput, dtype=jnp.float32)
+    kernel = jnp.ones((window,), dtype=throughput_arr.dtype) / window
+
+    # Equivalent to the original range(window, len(throughput)) behavior
+    smoothed = jnp.convolve(throughput_arr, kernel, mode="valid")[:-1]
+    # smoothed = jnp.array(throughput)
+
+    #save the array
+
+    plt.figure(figsize=(10, 8))
+    plt.plot(jnp.arange(len(smoothed)), smoothed,linewidth=2, label=f"{agent_name}")
+    plt.xlabel("Steps", fontsize=14)
+    plt.ylabel("Average Throughput (Mbps)", fontsize=14)
+    plt.title(f"{agent_name}_4Level (Block Avg, w={window})", fontsize=16)
+    plt.legend(fontsize=12, loc="lower right", frameon=True)
+    plt.grid(True, which="both", linestyle=":", linewidth=0.7, alpha=0.7)
+    plt.tight_layout()
+    plt.show()
+
+   
+    print("-" * 30)
+    print("=" * 60)
